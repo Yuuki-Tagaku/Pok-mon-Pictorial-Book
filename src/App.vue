5f980f38e003664325a22fdca83d3ef5da755b20
Yuuki-Tagaku <gyaaaaao.0v0.555@gmail.com>
2022-12-23 23:35:20 +0900

    create Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from "./components/TheHeader.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="style">
+    <TheHeader class="header"> </TheHeader>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.style {
+  background-color: tomato;
+  border-bottom: 3px solid black;
 }
 </style>
